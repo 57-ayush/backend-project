97b1e72bd0fb63b4c17618b69d916e882cf2da7a
Ayush kashyap <kashyapayush57@gmail.com>
2026-06-28 16:20:14 +0530

    worked in models with user and video model

---
+++ src/models/user.model.js
@@ -0,0 +1,81 @@
+import mongoose, { Schema } from "mongoose";
+
+const userSchema= new Schema({
+    username:{
+        type:String,
+        required:true,
+        lowercase:true,
+        trim:true
+    },
+    email:{
+        type:String,
+        required:true,
+        lowercase:true,
+        trim:true
+    },
+    fullName:{
+        type:String,
+        required:true,
+        
+    },
+    avatar:{
+        type:String,
+        required:true
+    },
+    coverImage:{
+        type:string
+    },
+    watchHistory:[{
+        
+        type:Schema.types.ObjectId,
+        ref:"Video"
+    }],
+    password:{
+        type:String,
+        required:true, 
+    },
+    refreshToken:{
+        type:string
+    }
+
+
+},{timestamps:true})
+
+userSchema.pre("save", async function (next) {
+    if(this.isModified("password")) 
+        this.password= await bycrypt.hash(this.password,"10")
+})
+
+userSchema.methods.isPasswordCorrect= async function(password) {
+    return await bycrypt.compare(password,this.password)
+}
+
+userSchema.methods.generateAccessToken = function(){
+    return jwt.sign(
+        {
+            _id: this._id,
+            email: this.email,
+            username: this.username,
+            fullName: this.fullName
+        },
+        process.env.ACCESS_TOKEN_SECRET,
+        {
+            expiresIn: process.env.ACCESS_TOKEN_EXPIRY
+        }
+    )
+}
+userSchema.methods.generateRefreshToken = function(){
+    return jwt.sign(
+        {
+            _id: this._id,
+            
+        },
+        process.env.REFRESH_TOKEN_SECRET,
+        {
+            expiresIn: process.env.REFRESH_TOKEN_EXPIRY
+        }
+    )
+}
+
+
+export const User=mongoose.model("User",userSchema)
