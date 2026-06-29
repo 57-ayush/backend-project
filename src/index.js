e5e9a29d6ec169352187e2926327f4c6ca9225a2
Ayush kashyap <kashyapayush57@gmail.com>
2026-06-29 11:06:39 +0530

    multer and file upload

--- src/index.js
+++ src/index.js
@@ -9,3 +9,11 @@ import connectDB from "./db/index.js";
 
 
 connectDB()
+.then(()=>{
+    app.listen(process.env.PORT || 8000, ()=>{
+        console.log(`App Listening on port: ${process.env.PORT}`)
+    })
+})
+.catch((error)=>{
+    console.log('failed to connect with DB',error)
+})
